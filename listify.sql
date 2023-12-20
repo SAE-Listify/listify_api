561691de804970cfad60fbf280e89646a1206cd6
Titouan-Gacougnolle <97309583+Titouan-Gacougnolle@users.noreply.github.com>
2023-12-20 20:57:58 +0100

    Update listify.sql

--- listify.sql
+++ listify.sql
@@ -4,85 +4,105 @@ use listify_bdd;
 
 create table project
 (
-    project_id int          not null
+    project_id int auto_increment
         primary key,
-    name       varchar(255) null
-)
-    engine = InnoDB;
+    name       varchar(255) not null
+);
 
 create table repository
 (
-    repository_id int          not null
+    repository_id int auto_increment
         primary key,
-    project_id    int          null,
-    name          varchar(255) null,
-    constraint repository_ibfk_1
+    project_id    int          not null,
+    name          varchar(255) not null,
+    constraint project_id
         foreign key (project_id) references project (project_id)
-)
-    engine = InnoDB;
+            on update cascade on delete cascade
+);
+
+create index project_id_idx
+    on repository (project_id);
 
 create table task
 (
-    task_id       int          not null
+    task_id       int auto_increment
         primary key,
-    repository_id int          null,
-    name          varchar(255) null,
-    completed     tinyint(1)   null,
-    constraint task_ibfk_1
+    repository_id int          not null,
+    name          varchar(255) not null,
+    completed     tinyint(1)   not null,
+    date          date         not null,
+    owner         varchar(255) not null,
+    constraint repository_id
         foreign key (repository_id) references repository (repository_id)
-)
-    engine = InnoDB;
+            on update cascade on delete cascade
+);
 
 create table subtask
 (
-    subtask_id int          not null
+    subtask_id int auto_increment
         primary key,
-    task_id    int          null,
-    name       varchar(255) null,
-    completed  tinyint(1)   null,
-    constraint subtask_ibfk_1
+    task_id    int          not null,
+    name       varchar(255) not null,
+    completed  tinyint(1)   not null,
+    constraint task_id
         foreign key (task_id) references task (task_id)
-)
-    engine = InnoDB;
-    
-create index project_id
-    on repository (project_id);
-    
-create index repository_id
+            on update cascade on delete cascade
+);
+
+create index task_id_idx
+    on subtask (task_id);
+
+create index repository_id_idx
     on task (repository_id);
 
-create index task_id
-    on subtask (task_id);
 
 INSERT INTO listify_bdd.project (project_id, name) VALUES (1, 'Projet 1');
 INSERT INTO listify_bdd.project (project_id, name) VALUES (2, 'Projet 2');
-INSERT INTO listify_bdd.project (project_id, name) VALUES (3, 'Projet 3');
-INSERT INTO listify_bdd.project (project_id, name) VALUES (4, 'Projet 4');
+
 
 INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (1, 1, 'A faire');
 INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (2, 1, 'En cours');
-INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (3, 1, 'Terminé');
-INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (4, 2, 'Repo 1 2');
-INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (5, 3, 'Ménage');
-INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (6, 3, 'Cuisine');
-INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (7, 4, 'Test');
-INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (8, 4, 'Test 2');
+INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (3, 1, 'Termine');
+INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (4, 2, 'Repo 1');
+INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (5, 2, 'Repo 2');
+INSERT INTO listify_bdd.repository (repository_id, project_id, name) VALUES (6, 2, 'Repo 3');
 
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (1, 1, 'Tache 1', 0);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (2, 1, 'Tache 2 ', 0);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (3, 1, 'Tache 3', 0);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (4, 2, 'Tache 1 2', 0);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (5, 2, 'Tache 2 2', 0);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (6, 2, 'Tache 3 2', 0);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (7, 3, 'Tache 1 3', 1);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (8, 3, 'Tache 2 3', 1);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (9, 5, 'Aspirateur', 0);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (10, 6, 'Gateau', 0);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (11, 5, 'Serpillère', 1);
-INSERT INTO listify_bdd.task (task_id, repository_id, name, completed) VALUES (12, 6, 'Repas', 1);
 
-INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (1, 10, 'Farine', 0);
-INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (2, 10, 'Sucre', 0);
-INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (3, 10, 'Beure', 1);
-INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (4, 12, 'vaisselle', 1);
-INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (5, 12, 'table', 0);
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (9, 1, 'Menage', 0, '2023-12-22', 'Femme');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (10, 1, 'Cuisine', 0, '2023-12-22', 'Femme');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (11, 2, 'Devoirs', 0, '2023-12-22', 'Thomas');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (12, 2, 'Reviser', 0, '2023-12-22', 'Thomas');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (13, 3, 'Ranger', 1, '2023-12-18', 'Jules');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (14, 3, 'Faire les courses', 1, '2023-12-18', 'Jules');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (15, 4, 'Tache 1', 0, '2023-12-22', 'Louis');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (16, 4, 'Tache 2', 0, '2023-12-22', 'Louis');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (17, 5, 'Tache 3', 0, '2023-12-22', 'Mass');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (18, 5, 'Tache 4', 1, '2023-12-18', 'Mass');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (19, 6, 'TAche 5 ', 1, '2023-12-18', 'Rayne');
+INSERT INTO listify_bdd.task (task_id, repository_id, name, completed, date, owner) VALUES (20, 6, 'Tache 6', 1, '2023-12-18', 'Rayane');
+
+
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (2, 9, 'Aspirateur', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (3, 9, 'Balais', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (4, 10, 'Plat', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (5, 10, 'Dessert', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (6, 11, 'Dm de Maths', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (7, 11, 'Exos de Mura', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (8, 12, 'PDF de Bielmann', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (9, 12, 'Active Directory', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (10, 13, 'Bureau', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (11, 13, 'Chambre', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (12, 14, 'Des fruits', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (13, 14, 'De la viande', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (14, 15, 'Sous-tache 1', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (15, 15, 'Sous-tache 2', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (16, 16, 'Sous-tache 3', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (17, 16, 'Sous-tache 4', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (18, 17, 'Sous-tache 5', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (19, 17, 'Sous-tache 6', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (20, 18, 'Sous-tache 7', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (21, 18, 'Sous-tache 8', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (22, 19, 'Sous-tache 9', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (23, 19, 'Sous-tache 10', 1);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (24, 20, 'Sous-tache 11', 0);
+INSERT INTO listify_bdd.subtask (subtask_id, task_id, name, completed) VALUES (25, 20, 'Sous-tache 12', 1);
